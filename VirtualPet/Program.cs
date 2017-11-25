035f2b09b0ceabefc8572b13a21c399cebeadf9e
kevingilfether <kevingilfether@gmail.com>
2017-11-25 14:44:23 -0500

    Added some pet generator logic

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -14,6 +14,29 @@ namespace VirtualPet
             Random rand = new Random();
 
 
+
+            //Main Program Logic
+            do
+            {
+
+
+            }
+
+            while ()
+
+            Console.WriteLine("Have a great day!");
+
+
+
+        }
+        static void MenuWriter()
+        {
+            Console.WriteLine("1. Check Status");
+            Console.WriteLine("2. Pat head");
+            Console.WriteLine("3. Play video games");
+            Console.WriteLine("4. Overthrow the patriarchy");
+            Console.WriteLine("5. Feed");
+            Console.WriteLine("6. Quit");
         }
     }
 }
