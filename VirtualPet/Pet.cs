48da25167e155a84f299295b68909ce45246f3b2
kevingilfether <kevingilfether@gmail.com>
2017-11-25 12:04:45 -0500

    Added fields and properties for pet

---
+++ VirtualPet/Pet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Pet
+    {
+        //fields
+        private double weight;
+        private int powerLevel;
+        private string mood;
+
+
+        //Properties
+        public double Weight
+        {
+            get { return this.weight; }
+            set { this.weight = value; }
+        }
+
+        public int PowerLevel
+        {
+            get { return this.powerLevel; }
+            set { this.powerLevel = value; }
+        }
+
+        public string Mood
+        {
+            get { return this.mood; }
+            set { this.mood = value; }
+        }
+    }
+}
